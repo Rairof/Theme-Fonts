da5aa928b12c250e6d850dd8cfecb2690906e986
arth <122994355+iveurne@users.noreply.github.com>
2024-12-01 11:51:49 -0300

    Update Figtree-font-snippet.js

--- Figtree/Figtree-font-snippet.js
+++ Figtree/Figtree-font-snippet.js
@@ -10,16 +10,16 @@
         "ggsans-ExtraBoldItalic": "https://github.com/Rairof/Theme-Fonts/raw/main/Figtree/Figtree-ExtraBoldItalic.ttf?raw=1",
         "ggsans-Medium": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Medium.ttf?raw=1",
         "ggsans-MediumItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-MediumItalic.ttf?raw=1",
-        "ggsans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Normal.ttf?raw=1",
-        "ggsans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-NormalItalic.ttf?raw=1",
-        "ggsans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Medium.ttf?raw=1",
-        "ggsans-SemiboldItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-MediumItalic.ttf?raw=1",
+        "ggsans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Regular.ttf?raw=1",
+        "ggsans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Italic.ttf?raw=1",
+        "ggsans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-SemiBold.ttf?raw=1",
+        "ggsans-SemiboldItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-SemiBoldItalic.ttf?raw=1",
         "NotoSans-Bold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Bold.ttf?raw=1",
         "NotoSans-ExtraBold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-ExtraBold.ttf?raw=1",
         "NotoSans-Medium": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Medium.ttf?raw=1",
-        "NotoSans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Normal.ttf?raw=1",
-        "NotoSans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-NormalItalic.ttf?raw=1",
-        "NotoSans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Medium.ttf?raw=1",
-        "SourceCodePro-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Medium.ttf?raw=1"
+        "NotoSans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Regular.ttf?raw=1",
+        "NotoSans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-Italic.ttf?raw=1",
+        "NotoSans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-SemiBold.ttf?raw=1",
+        "SourceCodePro-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/Figtree-SemiBold.ttf?raw=1"
     }
           }
