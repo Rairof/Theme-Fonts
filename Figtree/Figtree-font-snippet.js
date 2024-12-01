a11918df151eb636d69a9ae81985b20e4a12b4c4
arth <122994355+iveurne@users.noreply.github.com>
2024-12-01 11:43:41 -0300

    Create Figtree-font-snippet.js

---
+++ Figtree/Figtree-font-snippet.js
@@ -0,0 +1,25 @@
+{
+    "spec": 1,
+    "name": "Figtree",
+    "previewText": "Google Sans but its better than Google Sans ;)",
+    "main": {
+        "ABCGintoNord-ExtraBold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-ExtraBold.ttf?raw=1",
+        "ggsans-Bold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Bold.ttf?raw=1",
+        "ggsans-BoldItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-BoldItalic.ttf?raw=1",
+        "ggsans-ExtraBold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-ExtraBold.ttf?raw=1",
+        "ggsans-ExtraBoldItalic": "https://github.com/Rairof/Theme-Fonts/raw/main/Figtree/GoogleSans-ExtraBoldItalic.ttf?raw=1",
+        "ggsans-Medium": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Medium.ttf?raw=1",
+        "ggsans-MediumItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-MediumItalic.ttf?raw=1",
+        "ggsans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Normal.ttf?raw=1",
+        "ggsans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-NormalItalic.ttf?raw=1",
+        "ggsans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Medium.ttf?raw=1",
+        "ggsans-SemiboldItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-MediumItalic.ttf?raw=1",
+        "NotoSans-Bold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Bold.ttf?raw=1",
+        "NotoSans-ExtraBold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-ExtraBold.ttf?raw=1",
+        "NotoSans-Medium": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Medium.ttf?raw=1",
+        "NotoSans-Normal": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Normal.ttf?raw=1",
+        "NotoSans-NormalItalic": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-NormalItalic.ttf?raw=1",
+        "NotoSans-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Medium.ttf?raw=1",
+        "SourceCodePro-Semibold": "https://github.com/iveurne/Theme-Fonts/raw/main/Figtree/GoogleSans-Medium.ttf?raw=1"
+    }
+          }
